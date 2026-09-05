95d55e8a8b383235ced5f3b5f4322fa94755914f
latihanCommit <yobopi8143@inupup.com>
2026-09-06 01:39:59 +0800

    update

---
+++ render-start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Jalankan migrasi database saat server menyala
+# (Hapus tanda '#' di bawah ini NANTI jika Anda sudah menghubungkan Render ke Database gratis seperti Supabase/Aiven)
+# php artisan migrate --force
+
+# Jalankan web server Apache
+apache2-foreground
