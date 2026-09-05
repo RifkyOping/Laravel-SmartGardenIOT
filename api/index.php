0867bcbdd53c38018c27f711e39486bbd1185c01
latihanCommit <yobopi8143@inupup.com>
2026-09-06 02:26:24 +0800

    update

--- api/index.php
+++ api/index.php
@@ -8,6 +8,7 @@ if (!is_dir($storagePath)) {
     mkdir($storagePath . '/framework/sessions', 0777, true);
     mkdir($storagePath . '/framework/views', 0777, true);
     mkdir($storagePath . '/logs', 0777, true);
+    mkdir('/tmp/bootstrap/cache', 0777, true);
 }
 
 $_ENV['APP_STORAGE'] = $storagePath;
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,6 +19,9 @@ return Application::configure(basePath: dirname(__DIR__))
         'path.storage' => function () {
             return $_ENV['APP_STORAGE'] ?? base_path('storage');
         },
+        'path.bootstrap.cache' => function () {
+            return '/tmp/bootstrap/cache';
+        }
     ])
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
